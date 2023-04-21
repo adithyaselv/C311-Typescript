ffc6f7d9bc028754016f97132dedabdf33746232
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2023-04-20 23:46:23 -0700

    Done DS implementation of K in interpretor

--- Lecture11_2.js
+++ Lecture11_2.js
@@ -65,3 +65,82 @@ console.log("Testing function valofcps");
 console.log(valofcps(exp1, initEnv, function (v) { return v; }).toString());
 var val2 = valofcps(exp2, initEnv, function (v) { return v; });
 val2 instanceof Function ? console.log(val2(new LambdaCalculus_1.Atom(25), function (v) { return v; }).toString()) : console.log(val2.toString());
+// Todo: generate a data structure version of the above code
+var MakeRatorK = /** @class */ (function () {
+    function MakeRatorK(exp, env, k) {
+        this.exp = exp;
+        this.env = env;
+        this.k = k;
+    }
+    return MakeRatorK;
+}());
+var MakeRandK = /** @class */ (function () {
+    function MakeRandK(rator, k) {
+        this.rator = rator;
+        this.k = k;
+    }
+    return MakeRandK;
+}());
+var InitK = /** @class */ (function () {
+    function InitK() {
+    }
+    return InitK;
+}());
+var makeClosureDS = function (exp, env) {
+    return function (x, lk) {
+        return valofcpsDS(exp.body, extendEnvDS(env, exp.arg, x), lk);
+    };
+};
+var extendEnvDS = function (env, arg, v) {
+    return function (y, ek) {
+        return y.val === arg.val ? v : applyEnvDS(env, y, ek);
+    };
+};
+var applyEnvDS = function (env, x, k) {
+    return env(x, k);
+};
+var initEnvDS = function (x, k) {
+    throw new Error("Unbound variable");
+};
+var applyKds;
+applyKds = function (c, v) {
+    if (c instanceof MakeRatorK) {
+        return valofcpsDS(c.exp.rand, c.env, new MakeRandK(v, c.k));
+    }
+    else if (c instanceof MakeRandK) {
+        if (c.rator instanceof Function) {
+            return c.rator(v, c.k);
+        }
+        else {
+            throw new Error("rator is not a lambda");
+        }
+    }
+    else if (c instanceof InitK) {
+        return v;
+    }
+    else {
+        throw new Error("Invalid input");
+    }
+};
+var valofcpsDS;
+valofcpsDS = function (exp, env, k) {
+    if (exp instanceof LambdaCalculus_1.Atom) {
+        return applyKds(k, exp);
+    }
+    else if (exp instanceof LambdaCalculus_1.Symbol) {
+        return applyEnvDS(env, exp, k);
+    }
+    else if (exp instanceof LambdaCalculus_1.Lambda) {
+        return applyKds(k, makeClosureDS(exp, env));
+    }
+    else if (exp instanceof LambdaCalculus_1.App) {
+        return valofcpsDS(exp.rator, env, new MakeRatorK(exp, env, k));
+    }
+    else {
+        throw new Error("Invalid input");
+    }
+};
+console.log("Testing function valofcpsDS");
+console.log(valofcpsDS(exp1, initEnvDS, new InitK()).toString());
+var val2ds = valofcpsDS(exp2, initEnvDS, new InitK());
+val2ds instanceof Function ? console.log(val2ds(new LambdaCalculus_1.Atom(25), new InitK()).toString()) : console.log(val2ds.toString());
--- Lecture11_2.ts
+++ Lecture11_2.ts
@@ -88,3 +88,88 @@ val2 instanceof Function ? console.log(val2(new Atom(25), (v: ValueK) => v).toSt
 
 // Todo: generate a data structure version of the above code
 
+class MakeRatorK {
+    constructor(public exp: App, public env: EnvKDS, public k: ContDs) {}
+}
+
+class MakeRandK {
+    constructor(public rator: ValueKDS, public k: ContDs) {}
+}
+
+class InitK {
+    constructor() {}
+}
+
+type ContDs = MakeRatorK | MakeRandK | InitK;
+
+type EnvKDS = (x: Symbol, ek:ContDs) => ValueKDS;
+
+type ClosureKDS = (x: ValueKDS, k: ContDs) => ValueKDS;
+
+type ValueKDS = Atom | ClosureKDS;
+
+let makeClosureDS = (exp: Lambda, env: EnvKDS) => {
+    return (x: ValueKDS, lk: ContDs) => {
+        return valofcpsDS(exp.body, extendEnvDS(env, exp.arg, x), lk);
+    }
+}
+
+let extendEnvDS = (env: EnvKDS, arg: Symbol, v: ValueKDS) => {
+    return (y: Symbol, ek:ContDs) => {
+        return y.val === arg.val ? v : applyEnvDS(env, y, ek);
+    }
+}
+
+let applyEnvDS = (env: EnvKDS, x: Symbol, k: ContDs) => {
+    return env(x, k);
+}
+
+let initEnvDS = (x: Symbol, k: ContDs) => {
+    throw new Error("Unbound variable");
+}
+
+let applyKds: (c: ContDs, v: ValueKDS) => ValueKDS
+applyKds = (c, v) => {
+    if(c instanceof MakeRatorK) {
+        return valofcpsDS(c.exp.rand, c.env, new MakeRandK(v, c.k));
+    }
+    else if(c instanceof MakeRandK) {
+        if(c.rator instanceof Function) {
+            return c.rator(v, c.k);
+        }
+        else {
+            throw new Error("rator is not a lambda");
+        }
+    }
+    else if(c instanceof InitK) {
+        return v;
+    }
+    else {
+        throw new Error("Invalid input");
+    }
+}
+
+
+let valofcpsDS: (exp: Exp, env: EnvKDS, k: ContDs) => ValueKDS
+valofcpsDS = (exp, env, k) => {
+    if(exp instanceof Atom) {
+        return applyKds(k, exp);
+    }
+    else if(exp instanceof Symbol) {
+        return applyEnvDS(env, exp, k);
+    }
+    else if(exp instanceof Lambda) {
+        return applyKds(k, makeClosureDS(exp, env));
+    }
+    else if(exp instanceof App) {
+        return valofcpsDS(exp.rator, env, new MakeRatorK(exp, env, k));
+    }
+    else {
+        throw new Error("Invalid input");
+    }
+}
+
+console.log("Testing function valofcpsDS");
+console.log(valofcpsDS(exp1, initEnvDS, new InitK()).toString());
+let val2ds = valofcpsDS(exp2, initEnvDS, new InitK());
+val2ds instanceof Function ? console.log(val2ds(new Atom(25), new InitK()).toString()) : console.log(val2ds.toString());
